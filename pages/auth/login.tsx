f18883a3bce85b04e6113f48caba1131e0d5482a
dushimeemma <dushimeemma@gmail.com>
2022-01-11 18:41:29 +0200

    setup(chore): github actions
        - add vercel setup
        - add actions setup
    
    [Delvers #ESL-1]

---
+++ pages/auth/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className='flex justify-center items-center h-screen w-screen'>
+      hello from login
+    </div>
+  );
+};
+
+export default Login;
