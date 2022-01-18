0c7b091dec5d4bbc7e8fda7ab52d531109f59285
Emmanuel Dushime <64155652+dushimeemma@users.noreply.github.com>
2022-01-18 18:23:44 +0200

    ui(feature): implement user interface (#3)
    - add booking functionality
        - add book functionality
    
    [Delvers #ESL-3]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
+import React, { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import { useRouter } from 'next/router';
 
 import store from '../store/store';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isAuth, setIsAuth] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    const checkToken = localStorage.getItem('token');
+    if (checkToken === null) {
+      router.push('/auth/login');
+    }
+    setIsAuth(true);
+  }, [isAuth]);
   return (
     <Provider store={store}>
       <Component {...pageProps} />
